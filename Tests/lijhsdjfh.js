e5f6e2b7d79a0b9b8451021de7f1006098f80fbd
azhmprd <73955243+AzhamProdLive@users.noreply.github.com>
2024-01-27 20:11:59 +0100

    Update: Updating files

---
+++ Tests/lijhsdjfh.js
@@ -0,0 +1,22 @@
+[jan]
+bg = "#E6212a4c"
+bg-alt = "#708491"
+fg = "#27fbfe"
+ac = "#4DD0E1"
+blue-arch = "#00c9fe"
+
+purple = "#6800d2"
+blue = "#19bffe"
+
+color_menu_border = "#ffffff"
+color_menu_bg = "#000000FF"
+color_norm_bg = "#ffffff"
+color_norm_fg = "#a5b6cf"
+color_sel_bg = "#1F222B"
+color_sel_fg = "#a5b6cf"
+color_sep_fg = "#56687E"
+
+
+
+
+
